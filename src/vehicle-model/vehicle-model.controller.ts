0ab1a72854bbf90d2ae8507d40cd3d202c6d9136
deanxx <deanxx999@gmail.com>
2021-09-06 16:55:40 +0900

    create vehicle model controller and module

--- src/vehicle-model/vehicle-model.controller.ts
+++ src/vehicle-model/vehicle-model.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Put, Query } from '@nestjs/common';
+import { VehicleModelDto } from './vehicle-model.dto';
+import { VehicleModelDocument } from './vehicle-model.schema';
+import { VehicleModelService } from './vehicle-model.service';
 
 @Controller('vehicle-model')
-export class VehicleModelController {}
+export class VehicleModelController {
+  constructor(private vehicleModelService: VehicleModelService) {}
+
+  @Post()
+  async create(@Body() vehicleModelDto: VehicleModelDto): Promise<VehicleModelDocument> {
+    return this.vehicleModelService.create(vehicleModelDto);
+  }
+
+  @Get('all')
+  async findAll(): Promise<VehicleModelDocument[]> {
+    return this.vehicleModelService.findAll();
+  }
+
+  @Get('by-id')
+  async findById(@Query('id') id: string): Promise<VehicleModelDocument> {
+    return this.vehicleModelService.findById(id);
+  }
+
+  @Get('by-model')
+  async findByModel(@Query('model') model: string): Promise<VehicleModelDocument> {
+    return this.vehicleModelService.findByModel(model);
+  }
+
+  @Put('by-id')
+  async updateById(@Query('id') id: string, @Body() inputDoc: object): Promise<VehicleModelDocument> {
+    return this.vehicleModelService.updateById(id, inputDoc);
+  }
+
+  @Delete('by-id')
+  async deleteById(@Query('id') id: string): Promise<VehicleModelDocument> {
+    return this.vehicleModelService.deleteById(id);
+  }
+}
--- src/vehicle-model/vehicle-model.module.ts
+++ src/vehicle-model/vehicle-model.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { VehicleModelController } from './vehicle-model.controller';
+import { VehicleModel, VehicleModelSchema } from './vehicle-model.schema';
+import { VehicleModelService } from './vehicle-model.service';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: VehicleModel.name, schema: VehicleModelSchema }])
+  ],
+  controllers: [VehicleModelController],
+  providers: [VehicleModelService],
+})
 export class VehicleModelModule {}
