1fcaea4732a486855e6042c4d659862e2c2105f6
deanxx <deanxx999@gmail.com>
2021-09-06 15:22:10 +0900

    create vehicle module

--- src/vehicle/vehicle.module.ts
+++ src/vehicle/vehicle.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { VehicleController } from './vehicle.controller';
+import { Vehicle, VehicleSchema } from './vehicle.schema';
+import { VehicleService } from './vehicle.service';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Vehicle.name, schema: VehicleSchema }]),
+  ],
+  controllers: [VehicleController],
+  providers: [VehicleService],
+})
 export class VehicleModule {}
