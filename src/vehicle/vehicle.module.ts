46888a2ae12152f319a12d204d65211c9cff85cb
deanxx <deanxx999@gmail.com>
2021-09-07 10:31:42 +0900

    import vehicle model module at vehicle module

--- src/vehicle/vehicle.module.ts
+++ src/vehicle/vehicle.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { VehicleModelModule } from 'src/vehicle-model/vehicle-model.module';
 import { VehicleController } from './vehicle.controller';
 import { Vehicle, VehicleSchema } from './vehicle.schema';
 import { VehicleService } from './vehicle.service';
@@ -7,6 +8,7 @@ import { VehicleService } from './vehicle.service';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Vehicle.name, schema: VehicleSchema }]),
+    VehicleModelModule
   ],
   controllers: [VehicleController],
   providers: [VehicleService],
