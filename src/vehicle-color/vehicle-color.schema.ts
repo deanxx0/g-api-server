e4268a19748d77c398d694e0c198b43344738ecf
deanxx <deanxx999@gmail.com>
2021-09-13 17:53:40 +0900

    add vehicle color schema

---
+++ src/vehicle-color/vehicle-color.schema.ts
@@ -0,0 +1,10 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema({ timestamps: true })
+export class VehicleColor {
+  @Prop()
+  color: string;
+}
+
+export type VehicleColorDocument = VehicleColor & Document;
+export const VehicleColorSchema = SchemaFactory.createForClass(VehicleColor);
