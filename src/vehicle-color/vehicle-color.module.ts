2a6262926878432d64c7306ee19b2adbb16a6bd2
deanxx <deanxx999@gmail.com>
2021-09-14 14:29:35 +0900

    add vehicle color module

--- src/vehicle-color/vehicle-color.module.ts
+++ src/vehicle-color/vehicle-color.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { VehicleColorController } from './vehicle-color.controller';
+import { VehicleColor, VehicleColorSchema } from './vehicle-color.schema';
+import { VehicleColorService } from './vehicle-color.service';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: VehicleColor.name, schema: VehicleColorSchema },
+    ]),
+  ],
+  controllers: [VehicleColorController],
+  providers: [VehicleColorService],
+  exports: [VehicleColorService],
+})
 export class VehicleColorModule {}
